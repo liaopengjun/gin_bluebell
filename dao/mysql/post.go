93ac9a2d267db3af3ac8a4bed2c9c35b53813e2a
liaopengjun <1337404942@qq.com>
2021-11-17 22:47:30 +0800

    2021-11-17

--- dao/mysql/post.go
+++ dao/mysql/post.go
@@ -25,12 +25,12 @@ func GetPostList(page,size int64) (posts []*models.Post,err error) {
 }
 
 func GetPostListByIDs(ids []string) (postList []*models.Post,err error) {
-	sqlStr := `select post_id,content,author_id,community_id,create_time from post where post_id in (?) order by FIND_IN_SET (post_id,?)`
+	sqlStr := `select post_id,content,author_id,title,community_id,create_time from post where post_id in (?) order by FIND_IN_SET (post_id,?)`
 	query,args,err := sqlx.In(sqlStr,ids,strings.Join(ids,","))
 	if err != nil{
 		return nil ,err
 	}
 	query = db.Rebind(query)
-	err = db.Select(&postList,query,args)
+	err = db.Select(&postList,query,args...)
 	return
 }
--- dao/redis/post.go
+++ dao/redis/post.go
@@ -1,7 +1,9 @@
 package redis
 
-import "gin_bluebell/models"
-
+import (
+	"gin_bluebell/models"
+	"go.uber.org/zap"
+)
 func GetPostIDsInOrder(p *models.ParamPostList) ([]string,error) {
 	//1.根据用户请求的order参数确定查询redis-key
 	key := getRedisKey(KeyPostTimeZSet)
@@ -11,5 +13,7 @@ func GetPostIDsInOrder(p *models.ParamPostList) ([]string,error) {
 	//2.查询的索引起点
 	start := (p.Page -1)* p.Size
 	end := start + p.Size -1
+	zap.L().Debug("redis range start -> end ",zap.Int64("start",start),zap.Int64("end",end))
+	//0-9 9-18 .....
 	return client.ZRevRange(key,start,end).Result()
 }
